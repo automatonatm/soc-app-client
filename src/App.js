5a6d099a248db4e270390fe2277d9d3b7dda8914
Automaton <automatonatm@gmail.com>
2020-08-23 00:14:53 +0100

    setting store in our App js is complete

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import jwtDecode from  'jwt-decode'
 
 import { MuiThemeProvider,  createMuiTheme } from '@material-ui/core/styles'
 
+
+import {SET_AUTHENTICATED} from './store/types'
+import {logoutUser, getUserData } from  './store/actions/userActions'
+import  axios from 'axios'
+
 //pages
 import home from './pages/home'
 import login from './pages/login'
@@ -27,16 +32,19 @@ import Theme from './util/theme'
 
 const theme = createMuiTheme(Theme)
 
-let authenticated;
+
 
 const token = localStorage.FBidToken
 if(token) {
     const  decodeToken = jwtDecode(token)
     if(decodeToken.exp * 1000 < Date.now()) {
+        store.dispatch(logoutUser())
         window.location.href =  '/login'
-        authenticated = false
+
     }else {
-        authenticated = true
+       store.dispatch({type: SET_AUTHENTICATED})
+        axios.defaults.headers.common['Authorization'] = token
+        store.dispatch(getUserData())
     }
 }
 
@@ -50,8 +58,8 @@ function App() {
                    <div className="container">
                        <Switch>
                            <Route exact path="/" component={home} />
-                           <AuthRoute exact path="/login" component={login} authenticated={authenticated} />
-                           <AuthRoute exact path="/signup" component={signup} authenticated={authenticated} />
+                           <AuthRoute exact path="/login" component={login}  />
+                           <AuthRoute exact path="/signup" component={signup}  />
                        </Switch>
                    </div>
                </Router>
